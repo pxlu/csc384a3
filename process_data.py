4e5678628c7aebcb062b55bfb8e5b5daa7de28cb
Peter Lu <peterlu94@gmail.com>
2016-12-03 17:38:30 -0500

    Updated script

--- process_data.py
+++ process_data.py
@@ -6,10 +6,11 @@ def main():
 	inputs_path = "data.csv"
 	with open(inputs_path, 'r') as csvfile:
 		data_reader = csv.DictReader(csvfile)
-		data_list = [a3c.ItemProfile(name=row['Name'],price=row['Price'],brand=row['Brand'],seasonal=row['Seasonal'] if row['Seasonal'] != '' else 'Any',category=row['Categories'],is_entertainment=row['Entertainment?'],is_indoor=row['Indoor?'],rec_age=row['Recommended Age'] if row['Recommended Age'] != '' else '0+') for row in data_reader]
+		data_list = [a3c.ItemProfile(name=row['Name'], price=row['Price'],brand=row['Brand'] if row['Brand'] != '' else 'None', seasonal=row['Seasonal'] if row['Seasonal'] != '' else 'Any',category=row['Categories'],is_entertainment=row['Entertainment?'],is_indoor=row['Indoor?'],rec_age=row['Recommended Age'] if row['Recommended Age'] != '' else '0+') for row in data_reader]
 
 	for data in data_list:
 		print(str(data))
+		# print(_detailed_info(data))
 
 if __name__ == "__main__":
 	main()
